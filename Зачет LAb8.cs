e8e22cc9a016d8a6ec025302237b7a77d3c77ec6
DESKTOP-6IOI2I3\Елена Третьякова <popovam03@gmail.com>
2022-02-09 16:26:52 +0300

    Добавьте файлы проекта.

---
+++ Зачет LAb8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LAb8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = Convert.ToDouble(Console.ReadLine());
+            double func = 1 / 3.0 + 2 * y / 7.0 - z / 2.0 + Math.Min(z, x);
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(func);
+                
+            
+        }
+    }
+}
